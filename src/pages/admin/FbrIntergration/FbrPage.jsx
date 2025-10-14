f2ea6635d9d6843b8d8f92ae95f636deb5ad081c
Ali Gates <hacktech877@gmail.com>
2025-10-14 12:48:51 +0500

    11

--- src/pages/admin/FbrIntergration/FbrPage.jsx
+++ src/pages/admin/FbrIntergration/FbrPage.jsx
@@ -53,25 +53,92 @@ const salesChildren = [
 
 const FbrPage = () => {
   return (
-    <div className="p-6">
+    <div>
       <CommanHeader />
-      <h1 className="text-2xl font-bold mb-6">Fbr Integration</h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-3">
-        {salesChildren.map((item) => (
-          <NavLink
-            key={item.to}
-            to={item.to}
-            className="flex flex-col items-center justify-center text-black hover:no-underline group transition-all duration-300 hover:bg-emerald-100 h-40 w-60 rounded-2xl"
-          >
-            <div className="text-4xl mb-2 text-black group-hover:text-green-500 transition-colors duration-300">
-              {item.icon}
+
+      <div
+        className="p-6 relative min-h-screen  bg-cover bg-center"
+        style={{ backgroundImage: "url('/images/sales-invoice1.jpg')" }}
+      >
+        {/* Transparent overlay */}
+        <div className="absolute inset-0 bg-black opacity-50  backdrop-blur-sm"></div>
+
+        {/* Content Layer */}
+        <div className="relative z-10">
+
+          <h1 className="text-2xl text-white font-bold mb-6">Functionalities</h1>
+
+          <div className="bg-gray-400 opacity-80 rounded-xl px-4 py-4">
+
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 ">
+              {salesChildren.map((item) => (
+                <NavLink
+                  key={item.to}
+                  to={item.to}
+                  className="flex flex-col items-center justify-center text-white hover:text-green-600 hover:no-underline group transition-all duration-300 hover:bg-emerald-100 h-32 w-60 rounded-2xl"
+                >
+                  <div className="text-4xl mb-2 text-white group-hover:text-green-700 transition-colors duration-300">
+                    {item.icon}
+                  </div>
+                  <h2 className="text-lg font-semibold text-center">
+                    {item.label}
+                  </h2>
+                </NavLink>
+              ))}
             </div>
-            <h2 className="text-lg font-semibold text-center">{item.label}</h2>
-          </NavLink>
-        ))}
+          </div>
+
+            {/* Reposts */}
+           <h1 className="mt-2 text-2xl text-white font-bold mb-6">Reports</h1>
+
+          <div className="bg-gray-400 opacity-80 rounded-xl px-4 py-4">
+
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 ">
+              {salesChildren.map((item) => (
+                <NavLink
+                  key={item.to}
+                  to={item.to}
+                  className="flex flex-col items-center justify-center text-white hover:text-green-600 hover:no-underline group transition-all duration-300 hover:bg-emerald-100 h-32 w-60 rounded-2xl"
+                >
+                  <div className="text-4xl mb-2 text-white group-hover:text-green-700 transition-colors duration-300">
+                    {item.icon}
+                  </div>
+                  <h2 className="text-lg font-semibold text-center">
+                    {item.label}
+                  </h2>
+                </NavLink>
+              ))}
+            </div>
+          </div>
+
+              {/* Setup */}
+              <h1 className="mt-2 text-2xl text-white font-bold mb-6">Setup</h1>
+
+          <div className="bg-gray-400 opacity-80 rounded-xl px-4 py-4">
+
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 ">
+              {salesChildren.map((item) => (
+                <NavLink
+                  key={item.to}
+                  to={item.to}
+                  className="flex flex-col items-center justify-center text-white hover:text-green-600 hover:no-underline group transition-all duration-300 hover:bg-emerald-100 h-32 w-60 rounded-2xl"
+                >
+                  <div className="text-4xl mb-2 text-white group-hover:text-green-700 transition-colors duration-300">
+                    {item.icon}
+                  </div>
+                  <h2 className="text-lg font-semibold text-center">
+                    {item.label}
+                  </h2>
+                </NavLink>
+              ))}
+            </div>
+          </div>
+          
+        </div>
       </div>
     </div>
   );
 };
+
 
 export default FbrPage;
